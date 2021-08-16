6fc53a03baa561ac9381119c5d6e017085a4637e
Junnosuke Tasaka <junnosuke0120@gmail.com>
2021-08-17 00:45:48 +0900

    add

--- Easy/jewelsAndStones.js
+++ Easy/jewelsAndStones.js
@@ -1,3 +1,8 @@
-const numJewelsInStones = () => {
-  
+const numJewelsInStones = (jewels, stones) => {
+  let count = 0;
+  const dividedStones = stones.split("");
+  dividedStones.forEach(stone => {
+    if (jewels.indexOf(stone) > -1) count++;
+  });
+  return count;
 };
